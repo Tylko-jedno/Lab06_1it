cfaa632ed759ddb7588535251baf1cc440ebdbff
Tylko-jedno <diogkek@gmail.com>
2024-10-13 13:19:51 +0300

    first master commit

---
+++ Lab06_1/Lab06_1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void iter(int* a, int* s, const int size) {
+    int n = 0;
+    for (int i = 0; i < size; i++) {
+        if (a[i] > 0 || !(a[i] % 3 == 0)) {
+            s += a[i];
+            a[i] = 0;
+            n++;
+        }
+    }
+    cout << "s = " << int(s) << endl;
+    cout << "n = " << n << endl;
+}
+
+void Create(int* a, const int size, const int Low, const int High)
+{
+    for (int i = 0; i < size; i++)
+        a[i] = Low + rand() % (High - Low + 1);
+}
+
+
+void Print(int* a, const int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << setw(4) << a[i];
+    cout << endl;
+}
+
+
+
+int main()
+{
+    int p = -10;
+    int k = 35;
+
+    const int n = 22;
+    int a[n];
+    
+    Create(a, n, p, k);
+    Print(a, 22);
+    iter(a, 0, 22);
+    Print(a, 22);
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab06_1/Lab06_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int a[22] = { 31,  11,  22, -6,  23,  28,  14,   0, -4,  28, -9,  29, -5,  27,  15,  21, -5,  18,  33, -2, -3,  12 };
+
+			iter(a, 0, 22);
+
+			int expected[22] = { 0, 0, 0, -6, 0, 0, 0, 0, 0, 0, -9, 0, 0, 0, 0, 0, 0, 0, 0, 0, -3, 0 };
+
+			for (int i = 0; i < 22; i++)
+			{
+				Assert::AreEqual(expected[i], a[i], L"Elements are not equal", LINE_INFO());
+			}
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int a[22];
+
+			Create(a, 22, -10, 35);
+
+			int expected[22] = { 31,  11,  22, -6,  23,  28,  14,   0, -4,  28, -9,  29, -5,  27,  15,  21, -5,  18,  33, -2, -3,  12 };
+
+			for (int i = 0; i < 22; i++)
+			{
+				Assert::AreEqual(expected[i], a[i], L"Elements are not equal", LINE_INFO());
+			}
+		}
+	};
+}
+
